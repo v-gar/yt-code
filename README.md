adf8a4fbf36b6a620e0ded9d11397a4eec8a6ca5
Viktor Garske <git@v-gar.de>
2019-08-17 23:10:38 +0200

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+## YT-Code
+
+This repository contains source code files of programs
+presented in my [YouTube videos](https://www.youtube.com/viktorgarske).
---
+++ blockchain/chain.py
@@ -0,0 +1,58 @@
+import hashlib
+
+class Block:
+    prev_hash = None
+    nonce = -1
+
+    data = None
+
+    current_hash = None
+
+    def __init__(self, prev_hash, data):
+        self.prev_hash = prev_hash
+        self.data = data
+
+    def __repr__(self):
+        return "Block {0} (prev. {1})".format(
+            self.current_hash,
+            self.prev_hash
+        )
+
+    @property
+    def block_string(self):
+        return "{0}-{1}-{2}".format(
+            self.prev_hash,
+            self.data,
+            self.nonce
+        )
+
+    def hash(self):
+        return hashlib.sha256(bytes(self.block_string, "utf-8")).hexdigest()
+
+    def mine(self, difficulty):
+        found = False
+        while not found:
+            self.nonce += 1
+            self.current_hash = self.hash()
+            if self.current_hash[0:difficulty] == "0" * difficulty:
+                found = True
+
+
+class Blockchain:
+    blocks = []
+    difficulty = 4
+
+    def add(self, block):
+        block.mine(self.difficulty)
+        self.blocks.append(block)
+
+    @property
+    def last_block(self):
+        return self.blocks[-1] if len(self.blocks) else None
+
+    def build_block(self, data):
+        if self.last_block:
+            block = Block(self.last_block.current_hash, data)
+            self.add(block)
+        else:
+            raise Exception("Missing Genesis block")
